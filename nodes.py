1c3689222f5824e2be73c596ec66a59137ae64b1
Hiram Deng <72371329+dzhsurf@users.noreply.github.com>
2025-06-30 06:10:56 -0700

    Remove nodes into init

--- nodes.py
+++ nodes.py
@@ -1,9 +1,1 @@
-from components.inworld_nodes.inworld_tts_node import InworldTTSNode
-from components.inworld_nodes.audio_save_node import InworldAudioSaveNode
-from components.inworld_nodes.audio_preview_node import InworldAudioPreviewNode
-
-NODE_CLASS_MAPPINGS = {
-    "InworldTTSNode": InworldTTSNode,
-    "InworldAudioSaveNode": InworldAudioSaveNode,
-    "InworldAudioPreviewNode": InworldAudioPreviewNode,
-}
+# 
